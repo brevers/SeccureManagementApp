8a46f41a003ed8060b8543580c5c7e0559d67224
Tamara DBre <breversemail@gmail.com>
2023-03-05 20:26:26 +0000

    tasks and projects done

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :status, inclusion: {in: ["not-started-yet", "on-going", "concluded" ]}
+
+  STATUS_PREFERENCES = [
+    ["TO BE DONE", "not-started-yet"],
+    ["ONGOING", "on-going"],
+    ["DONE", "not-started-yet"]
+  ]
 end
