9ff67ddf662056f6fe56054a5d689230ea9b0f2c
Tamara DBre <breversemail@gmail.com>
2023-03-05 16:56:56 +0000

    routes home

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :projects
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "project#index"
+  root to: "home#index"
 
   # Defines the root path route ("/")
   # root "articles#index"
