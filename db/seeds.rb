2f54cd6ad68ab0d118a0d6f8daf72e1ab9c0e404
Tamara DBre <breversemail@gmail.com>
2023-03-06 12:47:25 -0300

    Only create test users on dev environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,6 @@ tasks = [
   "Deploy"
 ]
 
-users = [
-  ["admin@example.com", User.roles[:admin]],
-  ["jane@example.com", User.roles[:manager]],
-  ["john@example.com", User.roles[:engineer]],
-  ["grace@example.com", User.roles[:qa]],
-]
-
 projects.each do |project_name|
   Project.find_or_create_by(name: project_name) do |project|
     project.organization = organization
@@ -33,11 +26,21 @@ projects.each do |project_name|
   end
 end
 
-users.each do |email, role|
-  User.find_or_create_by(email: email) do |user|
-    user.role = role
-    user.password = "Test1234!"
-    user.password_confirmation = "Test1234!"
-    user.organization = organization
+
+if Rails.env.development?
+  users = [
+    ["admin@example.com", User.roles[:admin]],
+    ["jane@example.com", User.roles[:manager]],
+    ["john@example.com", User.roles[:engineer]],
+    ["grace@example.com", User.roles[:qa]],
+  ]
+
+  users.each do |email, role|
+    User.find_or_create_by(email: email) do |user|
+      user.role = role
+      user.password = "Test1234!"
+      user.password_confirmation = "Test1234!"
+      user.organization = organization
+    end
   end
 end
