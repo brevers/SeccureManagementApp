75083e19d76bceb00dc2fea44dcb9d15c5fba8ea
Tamara DBre <breversemail@gmail.com>
2023-03-06 12:47:25 -0300

    set default organization

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  before_validation :set_organization
 
   belongs_to :organization
 
   enum :role, %i(admin manager engineer qa)
 
+
+
   def manager_or_above?
     self.admin? || self.manager?
+  end
+
+  private
+
+  def set_organization
+    self.organization = Organization.first
   end
 end
